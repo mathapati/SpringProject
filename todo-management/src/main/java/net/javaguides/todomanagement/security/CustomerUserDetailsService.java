a89debcf0db520bf2602070454c5a32ba134779c
vinayk <mathapativinu21@gmail.com>
2023-08-26 13:24:01 +0530

    Added security in pom.xml

---
+++ todo-management/src/main/java/net/javaguides/todomanagement/security/CustomerUserDetailsService.java
@@ -0,0 +1,12 @@
+package net.javaguides.todomanagement.security;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+public class CustomerUserDetailsService implements UserDetailsService {
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return null;
+    }
+}
